cc1dfa8f1beaa2b68cd1b92ed593ac6a1c92946a
Geferson-Clay <gefersonclay30@gmailcom>
2023-12-12 11:12:13 -0300

    Create index.js

---
+++ index.js/index.js
@@ -0,0 +1,34 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case "Mago":
+                ataque = "usou Magia";
+                break;
+            case "Assassino":
+                ataque = "usou Adaga";
+                break;
+            case "Bruxo":
+                ataque = "usou artes Bola de fogo";
+                break;
+            case "Druida":
+                ataque = "usou Enraizar";
+                break;
+            default:
+                ataque = "usou um ataque genérico";
+        }
+
+        const mensagem = `O ${this.tipo} ${this.nome} atacou usando ${ataque}`;
+        console.log(mensagem);
+    }
+}
+
+// Heroi Escolhido
+const heroi = new Heroi("Astarion", 200, "Assassino");
+heroi.atacar();
